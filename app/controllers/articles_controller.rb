24d41e653678c1305aeea3b543e4ee26429fb5f5
Saman Batool <samanbatool08@gmail.com>
2022-04-27 15:22:42 -0400

    flash messages and validations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,14 +9,18 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
-    @article.save 
-
-    redirect_to @article
-    # can also be written as: redirect_to article_path(@article)
+    if @article.save 
+      flash[:notice] = "Article was created successfully"
+     redirect_to @article
+     # can also be written as: redirect_to article_path(@article)
+    else 
+      render 'new'
+    end
     
   end
 end
