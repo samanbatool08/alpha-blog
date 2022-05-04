9af87a8c4b4d5b6af5c2ec5bcda5f9931c8a06d6
Saman Batool <samanbatool08@gmail.com>
2022-05-04 12:34:04 -0400

    style index and form views

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,11 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+# disable field with errors div setting
+
+
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
