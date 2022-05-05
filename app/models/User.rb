0aeb46401eef452b08e10e98a6ad2cd5b9c69154
Saman Batool <samanbatool08@gmail.com>
2022-05-05 09:03:30 -0400

    add one-to-many assocation

--- app/models/User.rb
+++ app/models/User.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: { case_sensitive: false }, length: {maximum: 105}, format: { with: VALID_EMAIL_REGEX}
 
+  has_many :articles
 end
